05f176108d69e4bb884b648b92fd46121906c43f
kentaroy47 <kentaroy@stanford.edu>
2018-11-07 19:05:15 -0800

    initial fully connected mnist gan

---
+++ fc-gan.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import torch
+from torch import nn, optim
+from torch.autograd.variable import Variable
+from torchvision import transforms, datasets
+
+from utils import Logger
+
+def mnist_data():
+    compose = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((.5, .5, .5), (.5, .5, .5))
+        ])
+    out_dir = './dataset'
+    return datasets.MNIST(root=out_dir, train=True, transform=compose, download=True)
+# Load data
+data = mnist_data()
+# Create loader with data, so that we can iterate over it
+data_loader = torch.utils.data.DataLoader(data, batch_size=100, shuffle=True)
+# Num batches
+num_batches = len(data_loader)
+
